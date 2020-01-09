ed422a0a22a77313abeb5c38c40696ee2ac8f38c
bagdeabhishek <bagdeabhishek@gmail.com>
2020-01-10 00:44:58 +0530

    "Added logging statements, also added commandline argument parser. Refactored code. "

--- TweetCrawler.py
+++ TweetCrawler.py
@@ -1,45 +1,91 @@
-import tweepy
-import psycopg2
-from bs4 import BeautifulSoup
-from collections import OrderedDict
-import sqlite3
-from preproc import *
-from tqdm import tqdm
+import argparse
 import csv
-from threading import Thread
+import getpass
+import sys
 from queue import Queue
+from threading import Thread
+
+from preproc import *
+
+
+# import psycopg2
+
+
+def get_credentials(authfile):
+    try:
+        credentials_list = []
+        with open(authfile) as f:
+            cr = csv.reader(f)
+            keys = next(cr)
+            for row in cr:
+                dct = {}
+                for i in range(len(row)):
+                    dct[keys[i]] = row[i]
+                credentials_list.append(dct)
+        return credentials_list
+    except Exception as e:
+        logging.error("Cannot read credentials from file")
+        sys.exit("Exiting Fatal Error")
+
+
+def pg_get_conn(database="abhishek", user="abhishek", password=""):
+    """Get Postgres connection for fakenews
+
+    Returns:
+        Connection object : returns Post gres connection object
+
+    Args:
+        database (str, optional): Name of database
+        user (str, optional): Name of User
+        password (str, optional): Password of user
+    """
+    try:
+        conn = psycopg2.connect(database="fakenews",
+                                user="fakenews", password="fnd", host='localhost', port='5432')
+        return conn
+    except Exception as e:
+        logging.error("Problem Authenticating PostGre connection: " + str(e))
+
 
 def init_twitter_API(dct):
-    consumer_key = dct['consumer_key']
-    consumer_secret = dct['consumer_secret']
-    access_token = dct['access_token']
-    access_token_secret = dct['access_token_secret']
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.auth.API(auth,wait_on_rate_limit=True)
-    return api
-def insert_into_db(item):
-        keys = item.keys()
-        values = [item[x] for x in keys]
-        try:
-            cur = self.conn.cursor()
-            cur.execute('insert into tweet_urls_crawled(%s) values %s;',(psycopg2.extensions.AsIs(','.join(keys)), tuple(values)))
-            cur.close()
-        except Exception as e:
-            logger.debug("Postgres Failed "+str(e))
-            return False
-        return True
-def crawl_twitter(curr_id,api,conn):
     try:
-        for post in tweepy.Cursor(api.user_timeline,id=curr_id,summary=False,tweet_mode="extended").items():
+        consumer_key = dct['consumer_key']
+        consumer_secret = dct['consumer_secret']
+        access_token = dct['access_token']
+        access_token_secret = dct['access_token_secret']
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.auth.API(auth, wait_on_rate_limit=True)
+    except Exception as e:
+        logging.error("Problem Authenticating twitter credentials: " + str(e))
+    return api
+
+
+def insert_into_db(item, conn):
+    keys = item.keys()
+    values = [item[x] for x in keys]
+    try:
+        cur = conn.cursor()
+        cur.execute('insert into tweet_urls_crawled(%s) values %s;',
+                    (psycopg2.extensions.AsIs(','.join(keys)), tuple(values)))
+        cur.close()
+    except Exception as e:
+        logging.error("Inserting into Database Failed: " + str(e))
+        return False
+    return True
+
+
+def crawl_twitter(curr_id, api, conn):
+    try:
+        for post in tweepy.Cursor(api.user_timeline, id=curr_id, summary=False, tweet_mode="extended").items():
             dc = OrderedDict()
             curr_post = post._json
             dc['tweet_from'] = curr_id
             dc['created_at'] = curr_post['created_at']
-            ent_status_dct = curr_post.get("entities",False)
+            ent_status_dct = curr_post.get("entities", False)
             if ent_status_dct:
                 dc['hashtags'] = [x['text'] for x in curr_post['entities']['hashtags']]
-                dc['urls'] = [x['expanded_url'] for x in curr_post['entities']['urls'] ]
+                dc['urls'] = [x['expanded_url'] for x in curr_post['entities']['urls']]
                 dc['user_mentions_id'] = [x['id'] for x in curr_post['entities']['user_mentions']]
                 if 'media' in ent_status_dct:
                     dc['media'] = [x['media_url_https'] for x in curr_post['entities']['media']]
@@ -48,52 +94,60 @@ def crawl_twitter(curr_id,api,conn):
             dc['favorite_count'] = curr_post['favorite_count']
             dc['text'] = curr_post['full_text']
             dc['id'] = curr_post['id']
-            dc['in_reply_to_screen_name']  = curr_post['in_reply_to_screen_name']
-            dc['in_reply_to_user_id']  = curr_post['in_reply_to_user_id']
-            dc['in_reply_to_status_id']  = curr_post['in_reply_to_status_id']
-            dc['retweet_count']  = curr_post['retweet_count']
-            rt_status_dct = curr_post.get('retweeted_status',False)
-    #         adding retweet information because it is important.
+            dc['in_reply_to_screen_name'] = curr_post['in_reply_to_screen_name']
+            dc['in_reply_to_user_id'] = curr_post['in_reply_to_user_id']
+            dc['in_reply_to_status_id'] = curr_post['in_reply_to_status_id']
+            dc['retweet_count'] = curr_post['retweet_count']
+            rt_status_dct = curr_post.get('retweeted_status', False)
+            #         adding retweet information because it is important.
             if rt_status_dct:
                 dc['retweeted_status_text'] = curr_post['retweeted_status']['full_text']
-                dc['retweeted_status_url'] = [x['expanded_url'] for x in curr_post['retweeted_status']['entities']['urls'] ]
+                dc['retweeted_status_url'] = [x['expanded_url'] for x in
+                                              curr_post['retweeted_status']['entities']['urls']]
                 dc['retweeted_status_id'] = curr_post['retweeted_status']['id']
                 dc['retweeted_status_user_name'] = curr_post['retweeted_status']['user']['name']
                 dc['retweeted_status_user_handle'] = curr_post['retweeted_status']['user']['screen_name']
-            insert_into_postgres(dc,conn)
+            insert_into_postgres(dc, conn)
     except Exception as e:
-        print(str(e))
+        logging.info("Can't crawl ID " + curr_id + " exception: " + str(e))
 
-ls = pickle.load(open('pickles/tweet_article27519.pkl','rb'))
-with pg_get_conn() as conn:
-    cur = conn.cursor()
-    cur.execute('Select distinct(tweet_from) from tweet_articles_tweepy');
-    ans = cur.fetchall()
-ans = [x[0] for x in ans]
-new_ls = list(set(ls) - set(ans))
 
-access_dct=[]
-with open('files/twitteraccesscodes.csv','r') as f:
-    cr = csv.reader(f)
-    keys = next(cr)
-    for row in cr:
-        dct={}
-        for i in range(len(row)):
-            dct[keys[i]] = row[i]
-        access_dct.append(dct)
-q = Queue()
-_ = [q.put(x) for x in new_ls]
-
-def process(q,api,conn):
+def process(q, api, conn):
     while True:
         curr_id = q.get()
-        crawl_twitter(curr_id,api,conn)
+        crawl_twitter(curr_id, api, conn)
         q.task_done()
+
+
+q = Queue()
+_ = [q.put(x) for x in new_ls]
 
 with pg_get_conn() as conn:
     conn.autocommit = True
     for dct in access_dct:
-        api=init_twitter_API(dct)
-        worker=Thread(target=process,args=(q,api,conn))
+        api = init_twitter_API(dct)
+        worker = Thread(target=process, args=(q, api, conn))
         # worker.setDaemon(True)
         worker.start()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Multi-Threaded crawler for crawling Twitter")
+    parser.add_argument("--authcsv", default=None,
+                        help="The path to the csv file cointaining authorization tokens for twitter")
+    parser.add_argument("--dbname", default=None, help="Postgres database in which to enter the crawled data")
+    parser.add_argument("--dbuser", default=None, help="User name for the Postgres database ")
+    parser.add_argument("--outputcsv", default="crawled.csv", help="Output CSV file in which to dump crawled data")
+    parser.add_argument("--threads", default=1, help="Number of threads to use for crawling")
+    args = parser.parse_args()
+    if bool(args.dbname) ^ bool(args.dbuser):  # check if only one of db parameter is set. Used XOR
+        parser.error("Both --dbname and --dbuser should be set, you've set only one of them")
+    if args.dbname:
+        pg_password = getpass.getpass(prompt="Please enter password for the user of the postgres database ")
+    if not parser.authcsv:
+        auth_dct = {'consumer_key': getpass.getpass(prompt="Enter the consumer key obtained from twitter"),
+                    'consumer_secret': getpass.getpass(prompt="Enter the consumer secret for twitter API"),
+                    'access_token': getpass.getpass(prompt="Enter the access token for twitter API"),
+                    'access_token_secret': getpass.getpass(prompt="Enter the access token secret for twitter API")}
+        credentials = [auth_dct]
+    else:
+        credentials = get_credentials(parser.authcsv)
---
+++ preproc.py
@@ -0,0 +1,405 @@
+import logging
+import os
+import pickle
+import sqlite3
+import urllib.request
+from collections import OrderedDict
+
+import networkx as nx
+import psycopg2
+import requests
+import tweepy
+from bs4 import BeautifulSoup
+
+
+def get_pickle(file):
+    """Get Pickle file
+
+    Args:
+        file (str): Location of file
+
+    Returns:
+        object: the object read
+    """
+    with open(file, 'rb') as f:
+        ls = pickle.load(f)
+    return ls
+
+
+def get_political_handles(list_file=['files/inc_handles.txt', 'files/bjp_handles.txt'], get_online=True):
+    """Get the political handles from files as well as socialbakers site
+    Returns:
+        list: list of twitter handles
+    Args:
+        list_file (list, optional): List of files from which to get handles
+        get_online (bool, optional): Whether or not to get info online
+    """
+    ls_fin = []
+    for i in range(1, 23):
+        url = "https://www.socialbakers.com/statistics/twitter/profiles/india/society/politics/page-" + \
+              str(i) + "/?showMoreList-from=1&do=platformList-renderAjax&json"
+        html = urllib.request.urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser')
+        intm = soup.find_all('h2')
+        for y in intm:
+            for x in y.find_all('span'):
+                ls_fin.append(x.text.split(
+                    '(')[-1].replace(')', '').replace('@', ''))
+    for file in list_file:
+        with open(file) as f:
+            for i in f:
+                if i:
+                    ls_fin.append(i.strip())
+    logging.info(str(len(ls_fin)) + " IDs crawled")
+    return (ls_fin)
+
+
+def init_twitter_API(consumer_key='PaotgdTaxqqNMqeV4dG0Vs3oM',
+                     consumer_secret='fhkkKgTg3yp2BDP7M1hKJDyo6Dmo3H7xZqoWiUrNkTR9B4it8M',
+                     access_token='173773260-gObDFYJNDu2SK3xesi8zkJxdJV2noZm6V5rip02Q',
+                     access_token_secret='IM5KIeytpkIjwKn5GIOQvUvflP6efR136HlvcJSeyneCX'):
+    """Initialize twitter API using keys
+
+    Returns:
+        tweepyAPI object: This object will be used for crawling using offical twitter API
+
+    Args:
+        consumer_key (str, optional): Consumer key obtained from twitter API
+        consumer_secret (str, optional): Consumer secret obtained from twitter API
+        access_token (str, optional): Access token obtained from twitter API
+        access_token_secret (str, optional): Access token secret obtained from twitter API
+    """
+    consumer_key = 'PaotgdTaxqqNMqeV4dG0Vs3oM'
+    consumer_secret = 'fhkkKgTg3yp2BDP7M1hKJDyo6Dmo3H7xZqoWiUrNkTR9B4it8M'
+    access_token = '173773260-gObDFYJNDu2SK3xesi8zkJxdJV2noZm6V5rip02Q'
+    access_token_secret = 'IM5KIeytpkIjwKn5GIOQvUvflP6efR136HlvcJSeyneCX'
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+
+def crawl_twitter(list_ids, api, wait_on_rate_limit=False):
+    """Crawl twitter using official twitter API. Inserts into postgres database
+
+    Args:
+        list_ids (list): List of twitter handles to be crawled
+        api (tweepyAPI object): Tweepy api object initialized appropriately
+        wait_on_rate_limit (bool, optional): wait on exhaustion of quota and continue once replenished
+
+    Returns:
+        list: list of dictionaries representing tweets
+
+    Deleted Parameters:
+        db (bool, optional): Insert into database
+    """
+
+    ldc = []
+    count = 0
+    try:
+        for curr_id in list_ids:
+            for post in tweepy.Cursor(api.user_timeline, id=curr_id, summary=False, tweet_mode="extended",
+                                      wait_on_rate_limit=wait_on_rate_limit).items():
+                dc = OrderedDict()
+                curr_post = post._json
+                dc['tweet_from'] = curr_id
+                dc['created_at'] = curr_post['created_at']
+                dc['hashtags'] = [x['text']
+                                  for x in curr_post['entities']['hashtags']]
+                dc['urls'] = [x['expanded_url']
+                              for x in curr_post['entities']['urls']]
+                dc['user_mentions_id'] = [x['id']
+                                          for x in curr_post['entities']['user_mentions']]
+                if 'media' in curr_post['entities']:
+                    dc['media'] = [x['media_url_https']
+                                   for x in curr_post['entities']['media']]
+                dc['user_mentions_name'] = [x['screen_name']
+                                            for x in curr_post['entities']['user_mentions']]
+                dc['origin_device'] = BeautifulSoup(
+                    curr_post['source'], 'html.parser').a.string
+                dc['favorite_count'] = curr_post['favorite_count']
+                dc['text'] = curr_post['full_text']
+                dc['id'] = curr_post['id']
+                dc['in_reply_to_screen_name'] = curr_post[
+                    'in_reply_to_screen_name']
+                dc['in_reply_to_user_id'] = curr_post['in_reply_to_user_id']
+                dc['in_reply_to_status_id'] = curr_post[
+                    'in_reply_to_status_id']
+                dc['retweet_count'] = curr_post['retweet_count']
+                #         adding retweet information because it is important.
+                if ('retweeted_status' in curr_post):
+                    dc['retweeted_status_text'] = curr_post[
+                        'retweeted_status']['full_text']
+                    dc['retweeted_status_url'] = [x['expanded_url']
+                                                  for x in curr_post['retweeted_status']['entities']['urls']]
+                    dc['retweeted_status_id'] = curr_post[
+                        'retweeted_status']['id']
+                    dc['retweeted_status_user_name'] = curr_post[
+                        'retweeted_status']['user']['name']
+                    dc['retweeted_status_user_handle'] = curr_post[
+                        'retweeted_status']['user']['screen_name']
+                ldc.append(dc)
+                count += 1
+    except Exception as twe:
+        print(str(twe))
+    print("Total count : " + str(count))
+    return (ldc)
+
+
+def initialize_sqlite(file='twitter.db'):
+    """Initialize SQLITE connection
+
+    Args:
+        file (str, optional): Location of sqlite file
+
+    Returns:
+        sqlite connection: Sqlite connection object
+    """
+    conn = sqlite3.connect(file)
+    return conn
+
+
+def preproc_db(ldc):
+    """Preprocess the crawled list of dicts from twitter API to insert into DB
+
+    Args:
+        ldc (list): List of dicts containing crawled data from twitter
+
+    Returns:
+        list: list fo dicts ready to be inserted in database
+    """
+    for dc in ldc:
+        if (isinstance(dc['hashtags'], str)):
+            print("Already PreProcessed")
+            return ldc
+        if 'retweeted_status_url' in dc:
+            dc['retweeted_status_url'] = ",".join(dc['retweeted_status_url'])
+        if 'hashtags' in dc and dc['hashtags']:
+            #         print(dc['hashtags'])
+            dc['hashtags'] = ",".join(dc['hashtags'])
+        else:
+            dc['hashtags'] = 'NULL'
+
+        if 'urls' in dc:
+            dc['urls'] = ",".join(dc['urls'])
+        else:
+            dc['urls'] = 'NULL'
+
+        if 'media' in dc:
+            dc['media'] = ",".join(dc['media'])
+        else:
+            dc['media'] = 'NULL'
+        if 'user_mentions_id' in dc:
+            dc['user_mentions_id'] = ','.join(
+                str(x) for x in (dc['user_mentions_id']))
+        else:
+            dc['user_mentions_id'] = 'NULL'
+        if 'user_mentions_name' in dc:
+            dc['user_mentions_name'] = ",".join(dc['user_mentions_name'])
+        else:
+            dc['user_mentions_name'] = 'NULL'
+    return (ldc)
+
+
+def insert_into_db_list(ldc):
+    count = 0
+    for dc in ldc:
+        try:
+            columns = ', '.join(dc.keys())
+            placeholders = ':' + ', :'.join(dc.keys())
+            query = 'INSERT INTO TWITTER (%s) VALUES (%s)' % (
+                columns, placeholders)
+            conn.execute(query, dc)
+        except sqlite3.IntegrityError as e:
+            count += 1
+    print("Duplicates: " + str(count))
+    conn.commit()
+
+
+def create_table_db(conn):
+    conn.execute('''CREATE TABLE TWITTER
+         (
+         id INT PRIMARY KEY NOT NULL,
+         tweet_from TEXT    NOT NULL,
+         created_at            BLOB     NOT NULL,
+         hashtags        TEXT,
+         urls         TEXT,
+         user_mentions_id TEXT,
+         media TEXT,
+         user_mentions_name TEXT,
+         origin_device TEXT,
+         favorite_count INT,
+         text TEXT,
+
+         in_reply_to_screen_name TEXT,
+         in_reply_to_user_id INT,
+         in_reply_to_status_id INT,
+         retweet_count INT,
+         retweeted_status_text TEXT,
+         retweeted_status_url BLOB,
+         retweeted_status_id INT,
+         retweeted_status_user_name TEXT,
+         retweeted_status_user_handle TEXT
+
+         );''')
+
+
+def construct_graph(edge_list, G=nx.DiGraph()):
+    for t in edge_list:
+        if (len(t) == 2):
+            if (G.has_edge(t[0], t[1])):
+                G[t[0]][t[1]]['weight'] += 1
+            else:
+                G.add_edge(t[0], t[1], weight=1)
+    return (G)
+
+
+def get_list_ids_to_crawl_next(combined_id, list_ids=[], idx=1):
+    new_list_to_crawl = []
+    for x in combined_id:
+        if (x[idx]):
+            for y in x[idx].split(','):
+                new_list_to_crawl.append(y)
+    if (list_ids):
+        new_list_to_crawl = list(set(new_list_to_crawl) - (set(list_ids)))
+    return (new_list_to_crawl)
+
+
+def crawl(combined_id):
+    for index, x in enumerate(combined_id):
+        if (x):
+            combined_id[index] = "from:" + x
+    if not combined_id[0]:
+        combined_id.pop(0)
+    succ_cnt = 0
+    for c in combined_id:
+        print("Current id -> " + c + "\n Success Count -> " + str(succ_cnt))
+        rs = os.system(
+            "cd ../TweetScraper; scrapy crawl TweetScraper -a query=\"" + c + "\"")
+        if (rs == 0):
+            id_crawled.append(c)
+            succ_cnt += 1
+    return (succ_cnt)
+
+
+def construct_edge_list(cong):
+    """
+    Constructs edge lists with two mappings user->user mapping and user-websource mapping
+
+    Paramaters:
+        cong - list of tuples with 3 fields(user_from,user_mention,link)
+    returns:
+        user-user edge list, user-source edge
+    """
+    usr_to_src = []
+    list_to_exclude = [
+        'twitter',
+        'youtu',
+        'fllwrs',
+        'unfollowspy',
+        'livetv',
+        'pscp',
+        'live',
+        'ln.is',
+        'tinyurl',
+        'facebook',
+        'bit.ly',
+        'goo.gl',
+        'instagram',
+        'google'
+    ]
+    for x in cong:
+        if x[2]:
+            for url in x[2].split(','):
+                if not any(y in url for y in list_to_exclude) and x[0] not in url.replace('.', '_'):
+                    if url.endswith('.com') and url.startswith("http://www"):
+                        usr_to_src.append((x[0], url.split('.')[1].lower()))
+                    elif url.endswith('.com') and url.startswith("http://m"):
+                        usr_to_src.append((x[0], url.split('.')[1].lower()))
+                    elif url.endswith('.in') and url.startswith("http://www"):
+                        usr_to_src.append((x[0], url.split('.')[1].lower()))
+                    elif url.startswith("http://") or url.startswith("https://"):
+                        l_url = url.split('/')
+                        if len(l_url) >= 3 and '.' in l_url[2]:
+                            if l_url[2].startswith('www') or l_url[2].startswith('m'):
+                                usr_to_src.append(
+                                    (x[0], l_url[2].split('.')[1].lower()))
+                            else:
+                                usr_to_src.append((x[0], l_url[2].lower()))
+
+    ll = []
+    for i in cong:
+        if i[1]:
+            for x in i[1].split(','):
+                if (x != '@'):
+                    x = x.replace('@', '')
+                    ll.append((i[0], x))
+    return (ll, usr_to_src)
+
+
+def unshorten_url(url):
+    return requests.head(url, allow_redirects=True).url
+
+
+def pg_get_conn(database="fakenews", user="fakenews", password="fnd"):
+    """Get Postgres connection for fakenews
+
+    Returns:
+        Connection object : returns Post gres connection object
+
+    Args:
+        database (str, optional): Name of database
+        user (str, optional): Name of User
+        password (str, optional): Password of user
+    """
+    try:
+        conn = psycopg2.connect(database="fakenews",
+                                user="fakenews", password="fnd", host='localhost', port='5432')
+        return conn
+    except Exception as e:
+        print(str(e))
+
+
+def insert_into_postgres(item, conn):
+    keys = list(item.keys())
+    values = [item[x] for x in keys]
+    #     print(keys,values)
+    try:
+        cur = conn.cursor()
+        cur.execute('insert into tweet_articles_tweepy(%s) values %s;',
+                    (psycopg2.extensions.AsIs(','.join(keys)), tuple(values)))
+        cur.close()
+    except Exception as e:
+        print("Postgres Failed " + str(e))
+        #         print(keys,values)
+        return False
+    return True
+
+
+def get_clean_urls(text_list, list_to_exclude=['twitter']):
+    """
+    Get clean URLS from tweet texts from TweetScraper, takes into account urls broken by intermittent spaces
+    Parameters:
+    text_list - List of tweets containing tuples of (id, text)
+    list_to_exclude - List of sites to exclude
+    Returns:
+    ans_ls - List of tuples of type (id,urls)
+    """
+    ans_ls = []
+    for x in text_list:
+        rex = re.findall(
+            '(?:http:|https:)\/\/.*\/.*?(?:\.cms|\.[a-zA-Z]*|\/[a-zA-Z0-9-\ ]+[a-zA-z0-9])', x[1])
+        for rx in rex:
+            if rx and not any(z in rx for z in
+                              list_to_exclude) and not rx == 'http://' and not rx == 'https://' and not rx.endswith(
+                '.') and 't.c' not in rx:
+                if '\xa0' in x[1]:
+                    for y in x[1].split('\xa0'):
+                        #             print(x[0],y)
+                        ans_ls.append((x[0], y.replace(' ', '')))
+                elif '@' in x[1]:
+                    ans_ls.append((x[0], y.split('@')[0].replace(' ', '')))
+
+                else:
+                    ans_ls.append((x[0], x[1].replace(' ', '')))
+    return (ans_ls)
